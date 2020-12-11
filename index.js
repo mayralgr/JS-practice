b07624740a7be0f5a4374e1cf3db449ce24e86db
mayralgr <ml17ft@gmail.com>
2020-12-10 18:03:21 -0600

    Add db json

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 #! /user/bin/env node
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+readline.question(`What would you like to log today? `, async(item) => {
+    const { data } = await axios.get(`http://localhost:3001/food`);
+    const it = data[Symbol.iterator]();
+    let position = it.next();
+    while(!position.done) {
+        const food = position.value.name;
+        if ( food === item)
+        {
+            console.log(`${item} has ${position.value.calories} calories`);
+        }
+        position.next()
+    }
+    readline.close();
+})
