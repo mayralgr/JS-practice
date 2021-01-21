b2eb1b54ec2ac356c930f0fd3dfd2835bc22a2e6
mayralgr <ml17ft@gmail.com>
2021-01-21 10:44:46 -0600

    Add solution interviewmania

---
+++ interviewMania/sol1.js
@@ -0,0 +1,82 @@
+/*
+
+Given two strings s and t , write a function to determine if t is an anagram of s.
+You may assume the string contains only lowercase alphabets.
+
+Followup 
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+*/
+
+
+/**
+ *  Complexity O(n log n) --> sorting O( log n ), comparing O(n)
+ */
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+function isAnagram (s, t) {
+    let anagram = true;
+    if ( s.length !== t.length ) {
+        return false;
+    }
+    else {
+        let sCharArray = [...s];
+        let tCharArray = [...t];
+        sCharArray.sort();
+        tCharArray.sort();
+        for (let i = 0 ; i < sCharArray.length ; i++ ) {
+            if ( sCharArray[i] !== tCharArray[i] ) {
+                anagram = false;
+                break;
+            }
+        }
+    }
+    return anagram;
+};
+
+// using arrays  complexity => O (n) but wont work with unicode as the 26 array is fixed --> not so fast in ms
+function isAnagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    let counter = Array(26).fill(0) ; // length of the array is 26 for the abecedary 
+    for (let i = 0; i < s.length; i++) {
+        
+        console.log(s.charCodeAt(i) - 'a'.charCodeAt(0));
+        counter[s.charCodeAt(i) - 'a'.charCodeAt(0)]++; // sum in each of the letters
+        counter[t.charCodeAt(i) - 'a'.charCodeAt(0)]--; // rest in each to return to 0 if equals
+    }
+    // check the array
+    for (let i = 0; i < 26; i++) {
+        let count = counter[i];
+        if (count !== 0) { // not the same quantity, return false
+            return false;
+        }
+    }
+    return true;
+}
+
+// using hashmaps for unicode and any symbol complesty O(n) +++ faster
+function isAnagram( s, t) {
+    let smap        =new Map();
+    let slength     =s.length;
+    let tlength     =t.length;
+    if( slength !== tlength) {
+        return false;
+    }
+    for( let i=0 ; i<slength ; i++){
+        smap.set(s.charAt(i), (smap.get(s[i]) || 0) +1);
+        smap.set(t.charAt(i), (smap.get(t[i]) || 0) -1);
+    }
+    let isAnagram = true;
+    smap.forEach((value) => {
+        if ( value !== 0 ) {
+            isAnagram = false;
+            return;
+        }
+    });
+    return isAnagram;
+}
